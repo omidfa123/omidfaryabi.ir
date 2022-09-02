37be4710ad98ebb8f8e17d3681af7cdc61d5e581
omidfa <omidfa1234@gmail.com>
2022-09-02 15:25:56 -0700

    add resume

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div>omidfafsvsfvryabi webdeveloper</div>;
+  return (
+    <iframe
+      src="https://drive.google.com/file/d/1LGLLlKW9bVObo6Xji3wogI6WZLVVRWm-/preview"
+      style={{ width: '100%', height: '100vh', border: 'none' }}
+      allow="autoplay"
+    ></iframe>
+  );
 };
 
 export default Home;
